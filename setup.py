f4d794bfda8ce996fe9686308b547b6d677b9d78
Fentanest <better0101@naver.com>
2025-10-31 08:52:31 +0900

    1.1

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 import sys
 from cx_Freeze import setup, Executable
 
+# Define the version
+version = "1.1"
+
+# Determine OS for build directory name
+os_name = "Win" if sys.platform == "win32" else "Mac" if sys.platform == "darwin" else "Linux"
+build_dir = f"ExcelMerger v{version}-{os_name}"
+
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
     "packages": ["os", "sys", "re", "openpyxl", "xlrd", "msoffcrypto", "cffi", "cryptography"],
-    "includes": ["PySide6.QtCore", "PySide6.QtGui", "PySide6.QtWidgets"],
+    "includes": ["PySide6.QtCore", "PySide6.QtGui", '"PySide6.QtWidgets"'],
     "include_files": [("lib/logo.png", "lib/logo.png"), ("lib/logo.ico", "lib/logo.ico")],
-    "excludes": []
+    "excludes": [],
+    "build_dir": build_dir
 }
 
 # base="Win32GUI" should be used on Windows for a GUI application
@@ -16,7 +24,7 @@ if sys.platform == "win32":
 
 setup(
     name="ExcelMerger",
-    version="1.1",
+    version=version,
     description="Excel File Merger by Fentanest",
     options={"build_exe": build_exe_options},
     executables=[
