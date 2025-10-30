68029f5dc656266c4968755d96bf57bf612cd345
Fentanest <better0101@naver.com>
2025-10-30 19:42:54 +0900

    finish

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {
+    "packages": ["os", "sys", "re", "openpyxl", "xlrd", "msoffcrypto", "cffi", "cryptography"],
+    "includes": ["PySide6.QtCore", "PySide6.QtGui", "PySide6.QtWidgets"],
+    "include_files": ["config.ini", "secret.key", "ui/"],
+    "excludes": []
+}
+
+# base="Win32GUI" should be used on Windows for a GUI application
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+setup(
+    name="ExcelMerger",
+    version="1.0",
+    description="Excel File Merger",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main.py", base=base)],
+)
